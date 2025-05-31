a3e3810a134b77007ef20a0c2eda1b45a95bd062
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-05-31 15:08:18 +0200

    feat:Implement user edit functionality with controlled form and PUT request

--- src/pages/EditUser.jsx
+++ src/pages/EditUser.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react"
 import axios from "axios"
-import { useParams,useNavigate  } from "react-router-dom"
+import { useParams,Link  } from "react-router-dom"
 
 
 const EditUser = () => {
     const [isEditing,setIsEditing] = useState('')
     const {id} = useParams()
+
+    console.log(isEditing)
    
     const [name,setName] = useState('')
     const [lastname,setLastname] = useState('')
@@ -15,28 +17,41 @@ const EditUser = () => {
 
     const handleUpdateUser = async () => {
         try {
-            const response = await axios.put(`https://reqres.in/api/users/${id}`, user, {
+            const body = {
+                Name: name,
+                Lastname: lastname,
+                Email: email,
+                Position: job,
+              };
+
+            const response = await axios.put(`https://reqres.in/api/users/${id}`,body , {
                 headers: {
                     'x-api-key': 'reqres-free-v1'
                   }
             })
-            
+            console.log(response)
             setIsEditing(response)
+            setMessage('Usuario actualizado correctamente');
+            setName('')
+            setLastname('')
+            setEmail('')
+            setJob('')
 
 
         } catch (error) {
             console.error(error.message)
         }
     }
-
+    
     const handleSubmit = (event) =>{
         event.preventDefault()
+        handleUpdateUser()
        }
 
 
     return (
     <>
-     <form>
+     <form onSubmit={handleSubmit}>
         <input
             placeholder="Enter your name"
             value={name}
@@ -58,9 +73,10 @@ const EditUser = () => {
             onChange={(event) => setJob(event.target.value)}
                         
         />
+        <button><Link to={'/'}>Volver</Link></button>
+        <button type='submit' onClick={handleUpdateUser}>Editar</button>
      </form>
-     <button type='button' onClick={handleSubmit}>Editar</button>
-        {message && <p>{message}</p>}
+    {message && <p>{message}</p>}
         
     </>
     )
