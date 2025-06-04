8e33a1e84f2bbe5268398095a89ba327592fc5d9
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-06-04 10:45:14 +0200

    refactor: move API request bodies inside functions and fix undefined variables

--- src/services/reqRes.js
+++ src/services/reqRes.js
@@ -1,19 +1,54 @@
+import axios from "axios";
 
 
+const API_BASE_URL = "https://reqres.in/api";
+const HEADERS = {
+  "x-api-key": "reqres-free-v1",
+};
 
-// async function getUsers (pageNumber) {
-//       const response = await axios.get(`https://reqres.in/api/users?page=${pageNumber}` , {
-          
-//         headers: {
-//           'x-api-key': 'reqres-free-v1'
-//         }
-//         });
-
-
-//         return response
-
-// }
+const getUsers = async (pageNumber) => {
+  const response = await axios.get(`${API_BASE_URL}/users?page=${pageNumber}`, {
+    headers: HEADERS,
+  });
+  return response.data;
+};
 
 
+const createUser = async ({ name, lastname, email, job }) => {
+  const body = {
+    Name: name,
+    Lastname: lastname,
+    Email: email,
+    Position: job,
+  };
 
-// export default { getUsers, deleteUser }
+  const response = await axios.post(`${API_BASE_URL}/users`, body, {
+    headers: HEADERS,
+  });
+
+  return response.data;
+};
+
+const updateUser = async (id, { name, lastname, email, job }) => {
+    const body = {
+      Name: name,
+      Lastname: lastname,
+      Email: email,
+      Position: job,
+    };
+  
+    const response = await axios.put(`${API_BASE_URL}/users/${id}`, body, {
+      headers: HEADERS,
+    });
+  
+    return response.data;
+  };
+
+
+ const deleteUser = async (userId) => {
+  await axios.delete(`${API_BASE_URL}/users/${userId}`, {
+    headers: HEADERS,
+  });
+};
+
+export default {getUsers,createUser,deleteUser,updateUser}
