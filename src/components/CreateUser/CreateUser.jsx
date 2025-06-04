0f36ed3bdf833349d812c4e96ba0815ad3930e76
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-06-04 11:02:09 +0200

    fix:Refactor createUser logic to use external API service and remove validations

--- src/components/CreateUser/CreateUser.jsx
+++ src/components/CreateUser/CreateUser.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react"
-import axios from "axios"
 import { useNavigate } from "react-router-dom"
-import './createUser.css'
+import createUser  from '../../services/reqRes'
 import Button from '../Button/Button'
 import Input from '../Input/Input'
+import './createUser.css'
 
 const CreateUser = () => {
     const [name, setName] = useState('')
@@ -14,91 +14,37 @@ const CreateUser = () => {
     const [errors, setErrors] = useState({})
     const navigate = useNavigate();
 
-    const validate = () => {
-
-        // Los comentarios no son necesarios PORQUE el codigo tiene que ser AUTOEXPLICATIVO
-        // Evitamos en la medida de lo posible if-else-if
-        const newErrors = {};
-        // Validación nombre
-        if (!name.trim()) {
-            const messageNameEmpty = 'El nombre es obligatorio.'
-            newErrors.name = messageNameEmpty;
-        } else if (!/^[A-Za-zÁÉÍÓÚáéíóúÑñ ]{2,30}$/.test(name)) {
-            newErrors.name = 'El nombre solo puede contener letras y espacios (2-30 caracteres).';
-        }
-        // Validación apellido
-        if (!lastname.trim()) {
-            newErrors.lastname = 'El apellido es obligatorio.';
-        } else if (!/^[A-Za-zÁÉÍÓÚáéíóúÑñ ]{2,30}$/.test(lastname)) {
-            newErrors.lastname = 'El apellido solo puede contener letras y espacios (2-30 caracteres).';
-        }
-        // Validación empleo
-        // const minJobLength = 2
-        // const maxJoblength = 30
-        if (!job.trim()) {
-            newErrors.job = 'El empleo es obligatorio.';
-        } else if (job.length < 2 || job.length > 30) {
-            newErrors.job = 'El empleo debe tener entre 2 y 30 caracteres.';
-        }
-        // Validación email
-        if (!email.trim()) {
-            newErrors.email = 'El email es obligatorio.';
-        } else if (email.length > 50) {
-            newErrors.email = 'El email no puede tener más de 50 caracteres.';
-        } else if (!/^\S+@\S+\.\S+$/.test(email)) {
-            newErrors.email = 'El email debe ser válido (ejemplo@dominio.com).';
-        }
-        return newErrors;
+    const clearForm = () => {
+     setName('');
+     setLastname('');
+     setEmail('');
+     setJob('');
+     setErrors({});
     }
 
-   const clearForm = () => {
-    setName('');
-    setLastname('');
-    setEmail('');
-    setJob('');
-    setErrors({});
-    }
-
-    const createUser = async () => {
-        try {
-            const body = {
-                Name: name,
-                Lastname: lastname,
-                Email: email,
-                Position: job,
-            };
-            await axios.post(
-                "https://reqres.in/api/users/",
-                body,
-                {
-                    headers: {
-                        "x-api-key": "reqres-free-v1",
-                    },
-                }
-            );
-            clearForm()
-            setMessage('Usuario creado correctamente!');
-            
-        } catch (error) {
-            console.error(error.message);
-            setMessage('Error al crear el usuario');
-        }
-    };
-
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        const validationErrors = validate();
-        setErrors(validationErrors);
-        setMessage('');
-        if (Object.keys(validationErrors).length === 0) {
-            createUser();
+    
+        const body = {
+          name,
+          lastname,
+          email,
+          position: job,
+        };
+    
+        try {
+          await createUser.createUser(body);
+          clearForm();
+          setMessage('User created successfully!');
+        } catch (error) {
+          console.error(error.message);
+          setMessage('Error creating user');
         }
-    }
+      };
 
     return (
         <main className="createuser-main">
             <form onSubmit={handleSubmit} className="createuser-form">
-                {/* Parte del input, el container y la gestion de errores */}
                 <div className="input-container">
                     <Input
                         placeholder="Enter your name"
